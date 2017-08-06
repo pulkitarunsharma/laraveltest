fd762310ccd68175bbec2f2a6af2c3de4b14e5f7
pulkitarunsharma <pulkitarunsharma@users.noreply.github.com>
2017-08-06 13:33:56 +0530

    update index.php

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 echo "<h1>index.php file </h1>";
